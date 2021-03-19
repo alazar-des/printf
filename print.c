525ddd25d1ebee39d933b18509303c35773bba04
murageB <brendanrichard96@gmail.com>
2021-03-19 22:45:42 +0300

    Called to print anything

---
+++ print.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * _printf - the function a user called to print anything
+ * @format: foramt specifier or string just like printf
+ *
+ * Return: the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list  arg;
+	int counter = 0;
+       	struct WriteBuffer buffer;
+
+	va_start(arg, format);
+	buffer.index = 0;
+	while (*format)
+	{
+		if (*format == '\\')
+		{
+			handle_special_char(format, &buffer);
+			counter++;
+			format += 2;
+			continue;
+		}
+		if (*format != '%')
+		{
+			write_or_buffer(&buffer, *format);
+			counter++;
+			format++;
+			continue;
+		}
+		counter += handle_format_specifier(format, &buffer, &arg);
+	}
+	write(1, buffer.bufferArr, buffer.index);
+	return (counter);
+}
